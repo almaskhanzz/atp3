a7e086c94ca9d504b1dcbbebef6376c9a3a79559
Almas Khan <53224806+almaskhanzz@users.noreply.github.com>
2019-10-23 12:43:52 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,9 @@
+var express  	= require('express');
+var ejs  		= require('ejs');
+var bodyParse  	= require('body-parser');
+var exSession  	= require('express-session');
+var cookieParser= require('cookie-parser');
+var app 		= express();
+
+//CONGIFURATION
+app.set('view engine', 'ejs');
